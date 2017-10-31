7fd8a3b79490c6ba97aa25cf0bc4df1337de05f2
davidsolanes2 <davidsolanes2@gmail.com>
2017-10-31 19:35:46 +0100

    Initial application generated by JHipster-4.10.0

---
+++ src/main/webapp/app/admin/admin.state.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('rockBibleApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig ($stateProvider) {
+        $stateProvider.state('admin', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('rockBibleApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
